894de90d14eb9c859f1e0228e73cccdc73286ab5
G_JJG <59283807+wangdda9119@users.noreply.github.com>
2023-05-22 16:47:06 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,85 @@
+from flask import Flask, request, redirect
+#http://127.0.0.1:5000/
+app = Flask(__name__)
+
+nextId = 4
+topics = [
+    {"id" : 1, "title" : 'html', "body" : "html is..."},
+    {"id" : 2, "title" : 'css', "body" : "css is..."},
+    {"id" : 3, "title" : 'js', "body" : "js is..."}
+]
+
+def template(contents,content):
+    return f'''<!doctype html>
+    <html>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                {contents}
+            </ol>
+            {content}
+            <ul>
+                <li><a href="/create/"> create</a></li>
+            </ul>
+        </body>
+    </html>
+    '''
+
+def getContents():
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return liTags
+    
+@app.route('/')
+def index():
+    
+    return template(getContents(),'<h2> Welcome</h2>hello web')
+
+
+
+@app.route('/read/<int:id>')
+def read(id):
+    for topic in topics:
+        if id == topic["id"]:
+            title = topic['title']
+            body = topic['body']
+            break
+    
+    return template(getContents(), f'<h2>{title}</h2>{body}')
+
+
+
+
+
+@app.route('/create/', methods=['GET', 'POST'])
+def create():
+    if request.method == 'GET':
+        content = '''
+            <form action="/creat/" method="POST">
+                <p><input type="text" name="title" placeholder="title"></p>
+                <p><textarea name="body" placeholder="body"></textarea></p>
+                <p><input type="submit" value="create"></p>
+            </form>
+        '''
+        return template(getContents(), content)
+    
+    elif request.method == 'POST':
+        global nextId
+        title = request.form['title']
+        body = request.form['body']
+        newTopic = {'id' : nextId, 'title': title,'body':body }
+        topics.append(newTopic)
+        url = '/read/'+ str(nextId) +'/'
+        nextId += 1
+        return redirect(url)
+    
+    
+
+
+
+
+
+
+
+app.run(debug=True)
